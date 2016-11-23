0a4c63631376733f42a2f602910f98942eef8edf
Hien Tran <aznketchup@yahoo.com>
2016-11-23 15:20:07 -0800

    Initialize project "Hello World"

---
+++ Hello World/src/Hello World.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : Hien Tran
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello Wiener!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
