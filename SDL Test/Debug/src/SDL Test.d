97051a57dd20ebc82e6df03c375ffaf70f07398f
Hien Tran <aznketchup@yahoo.com>
2016-12-17 23:24:43 -0800

    Initialize "Hello World" project to test if SDL2 header was properly downloaded and sourced.

---
+++ SDL Test/Debug/src/SDL Test.d
@@ -0,0 +1,1 @@
+src/SDL\ Test.d: ../src/SDL\ Test.cpp
---
+++ SDL Test/Debug/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/SDL\ Test.cpp 
+
+OBJS += \
+./src/SDL\ Test.o 
+
+CPP_DEPS += \
+./src/SDL\ Test.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/SDL\ Test.o: ../src/SDL\ Test.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/SDL Test.d" -MT"src/SDL\ Test.d" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
