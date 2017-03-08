62181c6181b1f7a74b3e427f01a1dcf1c384920b
Hien Tran <aznketchup@yahoo.com>
2017-03-07 18:05:54 -0800

    Create a .dll and refence the .dll in a different project. -Initialize new project as a DLL application type name MathLibrary. -Declare namespace and class in .h file. -Define function of class in .h file. -Build MathLibrary. -Initialize new project as console application type name MathClient. -Reference MathLibrary folder. -Add MathLibrary directory to MathClient additional directories. -Include MathLibrary.h in MathClient.cpp. -Use functions declared in MathLibrary.

---
+++ DynamicLinkLibrary/MathLibrary/MathLibrary.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifdef MATHLIBRARY_EXPORTS
+#define MATHLIBRARY_API __declspec(dllexport)
+#else
+#define MATHLIBRARY_API __declspec(dllimport)
+#endif // MATHLIBRARY_EXPORTS
+
+namespace MathLibrary
+{
+	class Functions
+	{
+	public:
+		static MATHLIBRARY_API double Add(double a, double b);
+		static MATHLIBRARY_API double Multiply(double a, double b);
+		static MATHLIBRARY_API double AddMultiply(double a, double b);
+	};
+}
